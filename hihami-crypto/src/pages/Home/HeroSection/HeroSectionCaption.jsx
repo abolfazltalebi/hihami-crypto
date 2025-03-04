0e453549ec646a9a52b1dd7e91589f5f4529c17b
Your Name <you@example.com>
2025-03-04 17:35:16 +0330

    fix: adjust formatting in HeroSectionCaption component

--- hihami-crypto/src/pages/Home/HeroSection/HeroSectionCaption.jsx
+++ hihami-crypto/src/pages/Home/HeroSection/HeroSectionCaption.jsx
@@ -1,11 +1,12 @@
 import HeroSectionButton from "./HeroSectionButton";
 
 export default function HeroSectionCaption() {
-  return (
+  return ( 
     <div className="space-y-4">
       <h1 className="text-xl md:text-5xl leading-normal font-bold">
         EXPLORE, CREATE AND SELL YOUR NFT’S ON SHIHAMI
-      </h1>
+      </h1> 
+      
       <p className="text-foreground">
         Shihami is the World’s first and largest NFT/Avatar marketplace
       </p>
