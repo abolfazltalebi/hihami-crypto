57aa0b47ada81885e23cdd0368a768c718cec607
Your Name <you@example.com>
2025-03-04 16:45:21 +0330

    initialize React app with Vite, add routing, and create basic layout components

---
+++ hihami-crypto/src/pages/Home/HeroSection/HeroSectionImage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import nftHeroSection from "@/assets/images/nft-herosection.webp";
+
+export default function HeroSectionImage() {
+  return (
+    <div className="order-first">
+      <img src={nftHeroSection} className="w-full h-auto bg-cover" alt="" />
+    </div>
+  );
+}
